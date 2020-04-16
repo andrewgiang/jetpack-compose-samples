b6e0fefb5891a03bddc594aa777cf918a7845d6e
Andrew Giang <andrew.giang@detroitlabs.com>
2020-04-15 22:45:07 -0400

    initial commit

---
+++ app/src/main/java/com/detroitlabs/composeplayground/default/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.detroitlabs.composeplayground.default
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.Composable
+import androidx.ui.core.setContent
+import androidx.ui.foundation.Text
+import androidx.ui.material.MaterialTheme
+import androidx.ui.tooling.preview.Preview
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MaterialTheme {
+                Greeting("Android Group Hug!")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    MaterialTheme {
+        Greeting("Android")
+    }
+}
